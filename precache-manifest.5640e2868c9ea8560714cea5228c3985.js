4d1c8cef35bf7634475c87c2fac33bbe9a2543c8
volodpj <volod.af@gmail.com>
2019-06-06 12:42:45 +0300

    Updates

---
+++ precache-manifest.5640e2868c9ea8560714cea5228c3985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a6a2b5ba7e18b5a9c59ac047e916bb",
+    "url": "/phfone-react/index.html"
+  },
+  {
+    "revision": "1657006f50b835b7de8c",
+    "url": "/phfone-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/phfone-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1657006f50b835b7de8c",
+    "url": "/phfone-react/static/js/main.91b7bf2e.chunk.js"
+  },
+  {
+    "revision": "97f227dc8277188d36c8",
+    "url": "/phfone-react/static/js/runtime~main.83d69188.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/phfone-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
